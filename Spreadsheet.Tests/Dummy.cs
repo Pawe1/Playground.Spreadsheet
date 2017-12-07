778654e6f0c07da2efcc883d427fe36679b2e5b1
PC <Pawe1@users.noreply.github.com>
2017-12-07 17:15:29 +0100

    Changed folder structure; added draft of test suite, etc.

---
+++ Spreadsheet.Tests/Dummy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+ 
+namespace Spreadsheet.Tests
+{
+    [TestFixture]
+    public class SpreadsheetTestFixture
+    {
+        [Test]
+        public void Dummy()
+        {
+            Assert.That(true);
+        }
+    }
+}
--- SpreadsheetC/Spreadsheet.cs
+++ SpreadsheetC/Spreadsheet.cs
@@ -4,12 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpreadsheetC
+namespace Spreadsheet
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-        }
     }
 }
