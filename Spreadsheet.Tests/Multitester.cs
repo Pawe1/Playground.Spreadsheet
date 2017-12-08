d248b1f71679c6dc436c671c8177b82150aad258
PC <Pawe1@users.noreply.github.com>
2017-12-08 03:15:42 +0100

    Drafting...

--- Spreadsheet.Tests/Multitester.cs
+++ Spreadsheet.Tests/Multitester.cs
@@ -5,15 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using PC.Spreadsheet;
- 
+
 namespace PC.Spreadsheet.Tests
 {
     [TestFixture]
     public class SpreadsheetTestFixture
     {
         [Test]
-        public void Dummy()
+        public void SimpleInput1()
         {
+            //     private readonly string[] lines = { " 1 | 3 | 45 | 3.4 |", " 2 | 2 | -213 | 12;" };
+            //Environment.NewLine
+
+            Spreadsheet sut = new Spreadsheet();
+            Assert.That(sut.ToString().Length > 0);
+        }
+
+        [Test]
+        public void SimpleInput2()
+        {
+            // private readonly string[] lines = { " 1 | 3 | 45 |", " A1 + A2 + 4 | 1 | B1 + 3;" };
+
+            Spreadsheet sut = new Spreadsheet();
+            Assert.That(sut.ToString().Length > 0);
+        }
+
+        [Test]
+        public void Expression()
+        {
+            // private readonly string expression = { ""A1+A3* B2"" };
+
             Spreadsheet sut = new Spreadsheet();
             Assert.That(sut.ToString().Length > 0);
         }
--- SpreadsheetC/Spreadsheet.cs
+++ SpreadsheetC/Spreadsheet.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace PC.Spreadsheet
 {
+    internal struct Cell
+    {
+        public string Formula;
+        public int Value;
+
+        public bool HasReferences()
+        {
+            throw new NotImplementedException();
+        }            
+    }    
+
     public class Spreadsheet
     {
+        const char _cellSeparator = '|';
+        const char _spreadsheetTerminator = ';';
+
+        public void load(string lines)
+        {
+            throw new NotImplementedException();
+        }
+
         public void print()
         {
-            
+            Console.WriteLine("Tadaaa!");
         }
     }
 }
