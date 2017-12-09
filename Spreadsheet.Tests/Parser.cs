e7f15375f03f0923e5666b634a873ea43749f067
PC <Pawe1@users.noreply.github.com>
2017-12-09 03:35:59 +0100

    Expression evaluation using shunting-yard algorithm / reverse polish notation

--- Spreadsheet.Tests/Parser.cs
+++ Spreadsheet.Tests/Parser.cs
@@ -9,11 +9,47 @@ namespace PC.Spreadsheet.Tests
 {
     [TestFixture]
     public class ParserTestFixture
-    {
+    {      
         [Test]
-        public void Simple_parsing()
+        public void Parser_Input()
         {
-            Assert.That(Parser.Parse("1+2") == 3);
+            Assert.DoesNotThrow(() => {
+                Parser.Evaluate("-4 + ( 11 - ( 3 * 2 ) ) / 2");
+            });
+        }
+
+        [TestCase("1 + 2", 3)]
+        [TestCase("1 - 2", -1)]
+        [TestCase("2 * 2", 4)]
+        [TestCase("3 / 4", 0.75)]
+        [TestCase("-1 + 2", 1)]
+        [TestCase("0,1 + 0,2", 0.3)]
+        [TestCase("-0,1 / 0,01", -10)]
+        public void Simple_parsing(string expression, double expectedResult)
+        {
+            double result = Parser.Evaluate(expression);
+            Assert.That(result == expectedResult);
+        }
+
+        [Test]
+        public void Parsing_with_priorities()
+        {
+            double result = Parser.Evaluate("1 + 2 * 3");
+            Assert.That(result == 7);
+        }
+
+        [Test]
+        public void Parenthesis()
+        {
+            double result = Parser.Evaluate("1 + ( 2 * 3 )");
+            Assert.That(result == 7);
+        }
+
+        [Test]
+        public void Advanced_parsing()
+        {
+            double result = Parser.Evaluate("-4,1 + ( ( ( 5 - 3 ) * 3 ) / 2 ) * 3");
+            Assert.That(result == 4.9);
         }
     }
 }
--- SpreadsheetC/Parser.cs
+++ SpreadsheetC/Parser.cs
@@ -10,23 +10,112 @@ namespace PC.Spreadsheet
 {
     public static class Parser
     {
-        private static Dictionary<char, Tuple<byte, BinaryFunction>> functions =  new Dictionary<char, Tuple<byte, BinaryFunction>>()
+
+
+        static Dictionary<string, Tuple<byte, BinaryFunction>> functions =  new Dictionary<string, Tuple<byte, BinaryFunction>>()
         {
-            { '+',  new Tuple<byte, BinaryFunction>( 2, (x, y) => x + y ) },
-            { '-', new Tuple<byte, BinaryFunction>( 2, (x, y) => x - y ) },
-            { '*', new Tuple<byte, BinaryFunction>( 1, (x, y) => x * y ) },
-            { '/', new Tuple<byte, BinaryFunction>( 1, (x, y) => x / y ) }
+            { "+", new Tuple<byte, BinaryFunction>( 1, (x, y) => x + y ) },
+            { "-", new Tuple<byte, BinaryFunction>( 1, (x, y) => x - y ) },
+            { "*", new Tuple<byte, BinaryFunction>( 2, (x, y) => x * y ) },
+            { "/", new Tuple<byte, BinaryFunction>( 2, (x, y) => x / y ) }
         };
 
-        public static int Parse(string x)
+        static bool isOperator(string x)
         {
-           // Stack<double>
+            return functions.ContainsKey(x);
+        }
 
-           // x.Count(x => x == '&');
+        static byte opreratorPriority(string operatorr)
+        {
+            if (operatorr.Equals("(") || operatorr.Equals(")"))
+            {
+                return 0;
+            }
+            else if (functions.ContainsKey(operatorr))
+            {
+                return functions[operatorr].Item1;
+            }
+            else
+                throw new Exception("Unknown operator");
+        }
 
+        static int precedence(string operator1, string operator2)
+        {
+            byte operator1priority = opreratorPriority(operator1);
+            byte operator2priority = opreratorPriority(operator2);
+            if (operator1priority > operator2priority) return 1;
+            if (operator1priority < operator2priority) return -1;
             return 0;
         }
 
+        private static string[] tokenize(string input)
+        {
+            return input.Split(' ');
+        }
+        
+        private static IEnumerable<string> infixNotationToPostfixNotation(string[] tokens)   // Shunting-yard algorithm
+        {
+            Stack<string> operators = new Stack<string>();
+            List<string> output = new List<string>();
+            double n;
 
+            foreach (string token in tokens)
+            {
+                if (double.TryParse(token, out n))
+                {
+                    output.Add(token);
+                }
+                if (token == "(")
+                {
+                    operators.Push(token);
+                }
+                if (token == ")")
+                {
+                    while (operators.Count != 0 && operators.Peek() != "(")
+                    {
+                        output.Add(operators.Pop());
+                    }
+                    operators.Pop();
+                }
+                if (isOperator(token))
+                {
+                    while (operators.Count != 0 && (precedence(operators.Peek(), token) > -1)) 
+                    {
+                        output.Add(operators.Pop());
+                    }
+                    operators.Push(token);
+                }
+            }
+            while (operators.Count != 0)
+            {
+                output.Add(operators.Pop());
+            }
+            return output;
+        }
+
+        public static double Evaluate(string x)
+        {
+            string[] tokens = tokenize(x);
+            IEnumerable<string> rpn = infixNotationToPostfixNotation(tokens);
+
+            Stack<string> stack = new Stack<string>();
+            double operand1;
+            double operand2;
+            foreach (string token in rpn)
+            {
+                if (isOperator(token))
+                {
+                    operand2 = double.Parse(stack.Pop());
+                    operand1 = double.Parse(stack.Pop());
+                    double functionResult = functions[token].Item2(operand1, operand2);
+                    stack.Push(functionResult.ToString());                    
+                }
+                else
+                {
+                    stack.Push(token);
+                }
+            }
+            return double.Parse(stack.Pop());
+        }
     }
 }
